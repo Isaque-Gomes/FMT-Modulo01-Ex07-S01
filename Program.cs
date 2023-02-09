d46e1f245fe12fdcda66c1eee094427fec8e69bb
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-09 12:09:49 -0300

    [M1S01] Ex 7 - Boletim 2.0
    [Estrutura de Repetição, Arrays]
    Utilizando como base o programa do boletim (Exercício 3), crie uma nova versão dele, mas desta vez obtenha o nome do aluno, a quantidade de notas que serão utilizadas para compor a média, e cada uma das notas. 
    
    Após as entradas o programa deve calcular as médias, somando todas as notas e dividindo pelo número de notas, validar se o aluno foi aprovado, ficou de recuperação ou reprovado, utilizando os mesmos critérios: 
    ●	Se nota maior ou igual 6 Aprovado 
    ●	Se nota maior ou igual 5, e menor que 6 : Recuperação 
    ●	Se nota menor que 5: Exibir -> Reprovado 
    
    E por fim o programa deve exibir o boletim do aluno, no formato: 
    Aluno: {Nome aluno}  
    N1: {nota1} 
    N2: {nota2} 
    N3 {Nota3}
    Nn: {Notan}
    Média: {Valor da media} - {Aprovado Recuperação ou Reprovado}
    
    Ex: 
    Saída: Qual o nome do aluno? 
    Entrada :  Vitor
    Saída: quantas notas serão usadas para compor a média? 
    Entrada: 2 
    Saída: Digite a 1a nota: 
    Entrada: 8
    Saída: Digite a 2a nota:
    Entrada: 4
    Saída : Aluno: Vitor
    N1: 8
    N2: 1
    Média: 6 - Aprovado

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Boletim 2.0 - obter nome do aluno, quantidade de notas e cada uma das notas.
+// Depois, calcular a média dos alunos e informar se foi aprovado, ficou em recuperação ou reprovado.
+float media = 0;
+string nomeAluno;
+int quantidadeNotas;
+float soma = 0;
+
+Console.WriteLine("Olá! Vamos descobrir se o aluno foi aprovado, reprovado ou ficou de recuperação na disciplina!");
+Console.WriteLine("Digite o nome do aluno:");
+nomeAluno = Console.ReadLine();
+Console.WriteLine("Digite a quantidade de notas para compor a média:");
+quantidadeNotas = int.Parse(Console.ReadLine());
+float[] notas = new float[quantidadeNotas];
+for (int i = 0; i < quantidadeNotas; i++){
+    Console.WriteLine("Digite a " + (i+1) + "ª nota:");
+    notas[i] = float.Parse(Console.ReadLine());
+}
+for (int i = 0; i < quantidadeNotas; i++){
+    soma += notas[i];    
+}
+
+media = (soma / quantidadeNotas);
+Console.WriteLine("Aluno: " + nomeAluno);
+
+if(media >= 6){
+    for (int i = 0; i < quantidadeNotas; i++){
+        Console.WriteLine("N" + (i+1) + ": " + notas[i]);
+    }
+    Console.WriteLine($"Média: {media.ToString("N")} - Aprovado!!");
+}
+else if (media < 6 && media >= 5){
+    for (int i = 0; i < quantidadeNotas; i++){
+        Console.WriteLine("N" + (i+1) + ": " + notas[i]);
+    }
+    Console.WriteLine($"Média: {media.ToString("N")} - Em recuperação.");        
+}   
+else{
+    for (int i = 0; i < quantidadeNotas; i++){
+        Console.WriteLine("N" + (i+1) + ": " + notas[i]);
+    }
+    Console.WriteLine($"Média: {media.ToString("N")} - Reprovado."); 
+}        
